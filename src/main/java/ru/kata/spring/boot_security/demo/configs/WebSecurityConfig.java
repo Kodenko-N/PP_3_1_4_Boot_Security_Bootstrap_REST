c386882c32145d384a3dd6ef4ed2a435f08e9fc5
Kodenko-N <kodenko.n@gmail.com>
2025-03-28 11:34:13 +0300

    Updated according mentor comments

--- src/main/java/ru/kata/spring/boot_security/demo/configs/WebSecurityConfig.java
+++ src/main/java/ru/kata/spring/boot_security/demo/configs/WebSecurityConfig.java
@@ -10,7 +10,6 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-//import ru.kata.spring.boot_security.demo.service.CustomUserDetailsService;
 import ru.kata.spring.boot_security.demo.service.UserService;
 import ru.kata.spring.boot_security.demo.service.UserServiceImp;
 
@@ -19,13 +18,10 @@ import ru.kata.spring.boot_security.demo.service.UserServiceImp;
 public class WebSecurityConfig {
 
     private final SuccessUserHandler successUserHandler;
-    //private final CustomUserDetailsService userDetailsService;
-   private final UserService userService;
+    private final UserService userService;
 
-    public WebSecurityConfig(SuccessUserHandler successUserHandler, UserService userService ) {
-         //public WebSecurityConfig(SuccessUserHandler successUserHandler, UserService userService, CustomUserDetailsService userDetailsService) {
+    public WebSecurityConfig(SuccessUserHandler successUserHandler, UserService userService) {
         this.successUserHandler = successUserHandler;
-        //this.userDetailsService = userDetailsService;
         this.userService = userService;
     }
 
@@ -43,7 +39,7 @@ public class WebSecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(
-                        auth -> auth.requestMatchers("/","/index","/403").permitAll()
+                        auth -> auth.requestMatchers("/", "/index", "/403").permitAll()
                                 .requestMatchers("/user").hasAnyAuthority("USER", "ADMIN")
                                 .requestMatchers("/**").hasAuthority("ADMIN")
                                 .anyRequest().authenticated()
@@ -63,7 +59,7 @@ public class WebSecurityConfig {
 
     //аутентификация из БД
     @Bean
-    public UserDetailsService userDetailsService(){
+    public UserDetailsService userDetailsService() {
         return new UserServiceImp();
     }
 }
